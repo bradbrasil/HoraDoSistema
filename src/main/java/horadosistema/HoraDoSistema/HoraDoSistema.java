000203a927ed09a3e114960ca2d1e0623185c602
bradbrasil <bradbrasil13@gmail.com>
2022-09-25 15:05:52 +0100

    Initial commit

---
+++ src/main/java/horadosistema/HoraDoSistema/HoraDoSistema.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package horadosistema.HoraDoSistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bradb
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+     Date relogio = new Date ();
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+    }
+
+   
+}
